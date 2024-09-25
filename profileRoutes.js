134d53e7e77254b5c77a9ca342292176b1d1a7ea
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-09-25 12:32:48 +0530

    added profileRoutes

--- profileRoutes.js
+++ profileRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authMiddleware = require('./authMiddleware');
-const db = require('./db');
+const db = require('./database');
 const router = express.Router();
 
 
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const authRoutes = require('./authRoutes')
+const todoRoutes = require('./todoRoutes')
+const profileRoutes = require('./profileRoutes')
 const app = express()
 
 app.use(cors())
@@ -14,3 +16,5 @@ app.listen(PORT,()=>{
 })
 
 app.use('/api/auth',authRoutes)
+app.use('/api/todos',todoRoutes)
+app.use('/api',profileRoutes)
